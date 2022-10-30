cca5029847e06346e5491eaa68b84960a16401d0
MerryGaming <nhoquenroi2002@gmail.com>
2022-10-30 15:38:25 +0700

    create: base redis

---
+++ manager-daily-plan/src/main/java/org/aibles/managerdailyplan/ManagerDailyPlanApplication.java
@@ -0,0 +1,13 @@
+package org.aibles.managerdailyplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerDailyPlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagerDailyPlanApplication.class, args);
+	}
+
+}

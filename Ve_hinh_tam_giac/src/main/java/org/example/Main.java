8cf726e30f53f75b5719c1a9009e4fe590f1223f
MerryGaming <nhoquenroi2002@gmail.com>
2023-01-10 11:07:05 +0700

    update: bt ve hinh tam giac

--- Ve_hinh_tam_giac/src/main/java/org/example/Main.java
+++ Ve_hinh_tam_giac/src/main/java/org/example/Main.java
@@ -6,18 +6,56 @@ public class Main {
 
   public static void main(String[] args) {
 
-    Scanner scanner = new Scanner(System.in);
-    System.out.printf("Nhap so dong cua tam gia: ");
-    int n = scanner.nextInt();
+//    Scanner scanner = new Scanner(System.in);
+//    System.out.printf("Nhap so dong cua tam gia: ");
+//    int n = scanner.nextInt();
+//
+//    StringBuilder stringBuilder = new StringBuilder();
+//    stringBuilder.append("* ");
+//    String ve = stringBuilder.toString();
+//
+//    for (int i = 1; i <= n; i++) {
+//      for (int j = 1; j <= n-i; j++) {
+//        System.out.printf(" ");
+//      }
+//      for (int j = 1; j <= i; j++) {
+//        System.out.printf(ve);
+//      }
+//      System.out.printf("\n");
+//    }
 
-    for (int i = 1; i <= n; i++) {
-      for (int j = 1; j <= n-i; j++) {
-        System.out.printf(" ");
+    int n;
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("enter: ");
+    n = sc.nextInt();
+
+    for (int index = 0; index < n; index ++) {
+      for (int j = 0; j < n; j ++) {
+        System.out.print(" * ");
       }
-      for (int j = 1; j <= i; j++) {
-        System.out.printf("* ");
+      System.out.println();
+    }
+
+
+    System.out.println("--------------------------");
+    String row = "";
+    for (int index = 0; index < n; index ++) {
+      for (int j = 0; j < n; j ++) {
+        row = row + " * ";
       }
-      System.out.printf("\n");
+      System.out.println(row);
+      row = "";
+    }
+
+    System.out.println("--------------------------");
+    StringBuilder stringBuilder;
+    for (int index = 0; index < n; index ++) {
+      stringBuilder = new StringBuilder();
+      for (int j = 0; j < n; j ++) {
+        stringBuilder.append(" * ");
+      }
+      System.out.println(stringBuilder.toString());
     }
 
   }

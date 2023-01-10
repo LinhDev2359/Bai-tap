a204e3132dd63c211c792cd1a8d8b694820a1ce9
MerryGaming <nhoquenroi2002@gmail.com>
2023-01-10 11:09:54 +0700

    update: bt ioc and di

---
+++ ioc_and_di/src/main/java/org/aibles/ioc_and_di/IocAndDiApplication.java
@@ -0,0 +1,13 @@
+package org.aibles.ioc_and_di;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IocAndDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IocAndDiApplication.class, args);
+	}
+
+}
---
+++ ioc_and_di/src/test/java/org/aibles/ioc_and_di/IocAndDiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aibles.ioc_and_di;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IocAndDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

5f731de94094b0279c51c6b7681dbbb0fcf0f09d
MerryGaming <nhoquenroi2002@gmail.com>
2023-03-20 08:37:22 +0700

    create: Create giai cuu cong chua

--- giai_cuu_cong_chua/src/main/java/org/example/Main.java
+++ giai_cuu_cong_chua/src/main/java/org/example/Main.java
@@ -1,68 +1,52 @@
 package org.example;
 
-import java.util.Scanner;
+import java.util.Queue;
 
 public class Main {
 
-  public static Scanner scanner = new Scanner(System.in);
-
-  public static int inputNumberOfVillages() {
-    int numberOfVillages;
-    do {
-      System.out.printf("\nNhap so ngoi lang: ");
-      numberOfVillages = scanner.nextInt();
-      if (numberOfVillages < 1 || numberOfVillages > 4000) {
-        System.out.printf("Nhap sai, yeu cau nhap lai!!\n");
-      }
-    }while (numberOfVillages < 1 || numberOfVillages > 4000);
-    return numberOfVillages;
-  }
-
-  public static void run() {
-    int numberOfVillages;
-    int villageX = 0, villageY = 0;
-    int cost = 0;
-    int distance = 0;
-
-    numberOfVillages = inputNumberOfVillages();
-
-    for (int index = 1; index < numberOfVillages; index++) {
-      do {
-        System.out.printf("\nNhap ngoi lang X: ");
-        villageX = scanner.nextInt();
-        if (villageX < 1) {
-          System.out.printf("Nhap sai, yeu cau nhap lai!!");
-        }
-      }while (villageX < 1);
-
-      do {
-        System.out.printf("Nhap ngoi lang Y: ");
-        villageY = scanner.nextInt();
-        if (villageY > numberOfVillages && villageY != villageY) {
-          System.out.printf("Nhap sai, yeu cau nhap lai!!\n");
-        }
-      }while (villageY > numberOfVillages);
-
-      System.out.printf("Nhap chi phi cua hành trinh: ");
-      cost = scanner.nextInt();
-    }
-
-
-  }
-
   public static void main(String[] args) {
 
-   run();
+    int[] a = {1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int count = 0, count1 = 0, count2 = 0, count3 = 0, count4 = 0, count5 = 0, count6 = 0, count7 = 0, count8 = 0;
+    for (int index = 0; index < a.length; index++) {
+      if (a[index] == 1) {
+        count++;
+      }
+      if (a[index] == 2) {
+        count1++;
+      }
+      if (a[index] == 3) {
+        count2++;
+      }
+      if (a[index] == 4) {
+        count3++;
+      }
+      if (a[index] == 5) {
+        count4++;
+      }
+      if (a[index] == 6) {
+        count5++;
+      }
+      if (a[index] == 7) {
+        count6++;
+      }
+      if (a[index] == 8) {
+        count7++;
+      }
+      if (a[index] == 9) {
+        count8++;
+      }
 
-//    for (int index = 1; index < numberOfVillages; index++) {
-//        if (villageX == villageY && )
-//    }
-//
-//    int sum = sum + cost;
-//    if (){
-//      System.out.printf("Do dai cua hanh trinh dai nhat la: " + distance);
-//      System.out.printf("Tong chi phi cua hanh trinh do la: " + sum);
-//    }
+    }
+    System.out.printf("so 1 co " + count +" so");
+    System.out.printf("so 2 co " + count1 +" so");
+    System.out.printf("so 3 co " + count2 +" so");
+    System.out.printf("so 4 co " + count3 +" so");
+    System.out.printf("so 5 co " + count4 +" so");
+    System.out.printf("so 6 co " + count5 +" so");
+    System.out.printf("so 7 co " + count6 +" so");
+    System.out.printf("so 8 co " + count7 +" so");
+    System.out.printf("so 9 co " + count8 +" so");
 
   }
 }

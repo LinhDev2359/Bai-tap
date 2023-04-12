265a075c87b7c47f594a8ed94a73edf38b3dc20e
MerryGaming <nhoquenroi2002@gmail.com>
2023-04-12 08:48:11 +0700

    update: update zipkin service

--- zipkin/zipkin-client/src/main/java/org/aibles/zipkinclient/ZipkinClient1Controller.java
+++ zipkin/zipkin-client/src/main/java/org/aibles/zipkinclient/ZipkinClient1Controller.java
@@ -1,54 +1,32 @@
 package org.aibles.zipkinclient;
 
-import brave.Tracer;
 import brave.Span;
-import brave.Tracing;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import brave.Tracer;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import zipkin2.reporter.AsyncReporter;
-import zipkin2.reporter.Sender;
-//import zipkin2.Span;
-//import zipkin2.Tracer;
-
-import zipkin2.reporter.okhttp3.OkHttpSender;
-
 
 @RestController
 
 public class ZipkinClient1Controller {
 
+  private final Tracer tracer;
   @Autowired
   private RestTemplate restTemplate;
 
-  public ZipkinClient1Controller(RestTemplate restTemplate) {
+  public ZipkinClient1Controller(RestTemplate restTemplate, Tracer tracer) {
     this.restTemplate = restTemplate;
+    this.tracer = tracer;
   }
 
 
   @GetMapping("/hello")
   public String hello() {
-
-
-    Sender sender = OkHttpSender.create("http://localhost:9411/api/v2/spans");
-
-    Tracing tracing = Tracing.newBuilder()
-        .localServiceName("my-service1")
-        .spanReporter(AsyncReporter.create(sender))
-        .build();
-
-    Tracer tracer = tracing.tracer();
-
     Span newSpan = tracer.nextSpan().name("world span2").start();
-    try (Tracer.SpanInScope ws = tracer.withSpanInScope(newSpan)) {
+    try (Tracer.SpanInScope ws = tracer.withSpanInScope(newSpan.start())) {
       String response = restTemplate.getForObject("http://localhost:8081/greet", String.class);
+
       return "Greeting, " + response;
     } finally {
       newSpan.finish();

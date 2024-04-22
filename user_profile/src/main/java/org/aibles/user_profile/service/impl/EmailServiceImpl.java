dd821902e5ecdb42234f610b0d02dc680da20bdf
YaLynk <nhoquenroi2002@gmail.com>
2024-04-22 12:28:04 +0700

    update: add custom send file to mail

--- user_profile/src/main/java/org/aibles/user_profile/service/impl/EmailServiceImpl.java
+++ user_profile/src/main/java/org/aibles/user_profile/service/impl/EmailServiceImpl.java
@@ -1,13 +1,18 @@
 package org.aibles.user_profile.service.impl;
 
 import jakarta.mail.Message;
+import java.io.File;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aibles.user_profile.constant.EmailConstant;
 import org.aibles.user_profile.exception.InternalServerError;
 import org.aibles.user_profile.service.EmailService;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
@@ -28,7 +33,30 @@ public class EmailServiceImpl implements EmailService {
       message.setContent(getContent(template, properties), EmailConstant.CONTENT_TYPE_TEXT_HTML);
       emailSender.send(message);
     } catch (Exception ex) {
-      log.info("(send)subject: {}, to: {}, ex: {} ", subject, to, ex.getMessage());
+      log.error("(send)subject: {}, to: {}, ex: {} ", subject, to, ex.getMessage());
+      throw new InternalServerError();
+    }
+  }
+
+  @Override
+  public void sendd(String subject, String to, String content, List<String> attachments) {
+    log.info("(send)subject: {}, to: {}, content: {}, attachments: {}", subject, to, content, attachments);
+    try {
+      var message = emailSender.createMimeMessage();
+      MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+      helper.setTo(to);
+      helper.setSubject(subject);
+      helper.setText(content);
+
+      for (String filePath : attachments) {
+        FileSystemResource file = new FileSystemResource(new File(filePath));
+        helper.addAttachment(Objects.requireNonNull(file.getFilename()), file);
+      }
+
+      emailSender.send(message);
+    } catch (Exception ex) {
+      log.error("(send)subject: {}, to: {}, ex: {}", subject, to, ex.getMessage());
       throw new InternalServerError();
     }
   }
